4a26c74cf5c74ac52ca7829d29af954866947840
dejan <dejan.ercegan@gmail.com>
2019-06-02 21:38:34 +0200

    added dictionary

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import src.lists_tut as lists_tut
+import src.dictionary_tut as dictionary_tut
 
 
 def main():
-    upercase_list = lists_tut.change_to_uppercase(['dejan','Branislav'])
-    print(upercase_list)
+    upercase_list = lists_tut.change_to_uppercase(['dejan', 'Branislav'])
+
+    #######################################################################
+
+    input_dict = {
+        "Dejan" : "55555",
+        "Branislav" : "11111"
+    }
+
+    filter_dict = dictionary_tut.filter_dictionary(input_dict, "D")
+
+
 
 if __name__ == '__main__':
     main()
---
+++ src/dictionary_tut.py
@@ -0,0 +1,24 @@
+def filter_dictionary(arg_dict, filter_string):
+    """
+    This function filters received dictionary to contain only entries starting with second parameter.
+    Example
+    arg_dict
+    myDict = {
+        "Dejan" : "555555",
+        "Branislav" : "111111"
+    }
+    filter_string
+    "De"
+    {
+        "Dejan" : "555555"
+    }
+
+    :param arg_dict:
+    :param filter_string:
+    :return:
+    """
+    res = {}
+    print(res)
+
+    for element in elements
+    return res

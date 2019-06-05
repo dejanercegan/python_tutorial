06a0baef05dc4c6e1c8887f7d0a3c5c7d1a83ae4
dejan <dejan.ercegan@gmail.com>
2019-06-05 11:43:41 +0200

    ifelse

---
+++ src/ifelse_tut.py
@@ -0,0 +1,15 @@
+# return the list of random numbers to correct order
+
+def ifelse_numlist(arg_ifelse):
+    """
+    Changes all strings in a list to upercase
+    :param arg_list: List of random numbers
+    :return: List of random numbers in correct order
+    """
+    result = []
+
+    for num_list in arg_ifelse:
+        if 11 > 4 and 4 > -7:
+            print ([11, 4, -7])
+
+    return result

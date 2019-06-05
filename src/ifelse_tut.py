b7784b720e8382c0677986a034b5437a7b9b4376
dejan <dejan.ercegan@gmail.com>
2019-06-05 11:47:02 +0200

    ifelse1

--- src/ifelse_tut.py
+++ src/ifelse_tut.py
@@ -3,7 +3,7 @@
 def ifelse_numlist(arg_ifelse):
     """
     Changes all strings in a list to upercase
-    :param arg_list: List of random numbers
+    :param arg_ifelse: List of random numbers
     :return: List of random numbers in correct order
     """
     result = []

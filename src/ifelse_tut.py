fcaba7c4057f0d3ef2083c8313123b9ff955a890
dejan <dejan.ercegan@gmail.com>
2019-06-06 16:18:52 +0200

    ifelse_tut_1

--- src/ifelse_tut.py
+++ src/ifelse_tut.py
@@ -8,6 +8,10 @@ def ifelse_numlist(arg_ifelse):
     """
     result = []
 
+    x = int(input("1st num"))
+    y = int(input("2nd num"))
+    z = int(input("3rd num"))
+
     for num_list in arg_ifelse:
         if x > y and y > z:
             print (['x', 'y', 'z'])
